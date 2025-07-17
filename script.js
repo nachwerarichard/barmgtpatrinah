fa2c7606e253e77317a23717ca99783960789d5a
Nachwera Richard <87071510+nachwerarichard@users.noreply.github.com>
2025-07-17 11:39:39 +0300

    Updated by adding exporttabletoexcel in  script.js

--- script.js
+++ script.js
@@ -1400,6 +1400,34 @@ document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('generate-report-button').addEventListener('click', generateReports);
 });
 
+function exportTableToExcel(tableID, filename = '') {
+    const dataType = 'application/vnd.ms-excel';
+    const tableSelect = document.getElementById(tableID);
+    const tableHTML = tableSelect.outerHTML.replace(/ /g, '%20');
+
+    // Default filename
+    filename = filename ? filename + '.xls' : 'excel_data.xls';
+
+    // Create a download link element
+    const downloadLink = document.createElement("a");
+    document.body.appendChild(downloadLink);
+
+    if (navigator.msSaveOrOpenBlob) {
+        // For IE (older versions)
+        const blob = new Blob(['\ufeff', tableHTML], { type: dataType });
+        navigator.msSaveOrOpenBlob(blob, filename);
+    } else {
+        // For other browsers
+        downloadLink.href = 'data:' + dataType + ', ' + tableHTML;
+        downloadLink.download = filename;
+        downloadLink.click();
+    }
+
+    // Cleanup
+    document.body.removeChild(downloadLink);
+}
+
+
    
        
     
