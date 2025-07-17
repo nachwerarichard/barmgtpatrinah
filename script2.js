907642d7f9b233b209e8c2c13518370887e1b8e1
Nachwera Richard <87071510+nachwerarichard@users.noreply.github.com>
2025-07-17 11:34:12 +0300

    Updated script2.js

--- script2.js
+++ script2.js
@@ -1,4 +1,32 @@
-  
+function exportTableToExcel(tableID, filename = '') {
+    const dataType = 'application/vnd.ms-excel';
+    const tableSelect = document.getElementById(tableID);
+    const tableHTML = tableSelect.outerHTML.replace(/ /g, '%20');
+
+    // Default filename
+    filename = filename ? filename + '.xls' : 'excel_data.xls';
+
+    // Create a download link element
+    const downloadLink = document.createElement("a");
+    document.body.appendChild(downloadLink);
+
+    if (navigator.msSaveOrOpenBlob) {
+        // For IE (older versions)
+        const blob = new Blob(['\ufeff', tableHTML], { type: dataType });
+        navigator.msSaveOrOpenBlob(blob, filename);
+    } else {
+        // For other browsers
+        downloadLink.href = 'data:' + dataType + ', ' + tableHTML;
+        downloadLink.download = filename;
+        downloadLink.click();
+    }
+
+    // Cleanup
+    document.body.removeChild(downloadLink);
+}
+
+
+
         // Basic JavaScript for section display and menu toggle
         function showSection(sectionId) {
             // Remove 'active' from all sections
@@ -79,33 +107,6 @@
             /* Export to XLSX file */
             XLSX.writeFile(wb, filename + '.xlsx');
         }
-    </script>
-    <script>
-        function exportTableToExcel(tableID, filename = '') {
-    const dataType = 'application/vnd.ms-excel';
-    const tableSelect = document.getElementById(tableID);
-    const tableHTML = tableSelect.outerHTML.replace(/ /g, '%20');
-
-    // Default filename
-    filename = filename ? filename + '.xls' : 'excel_data.xls';
-
-    // Create a download link element
-    const downloadLink = document.createElement("a");
-    document.body.appendChild(downloadLink);
-
-    if (navigator.msSaveOrOpenBlob) {
-        // For IE (older versions)
-        const blob = new Blob(['\ufeff', tableHTML], { type: dataType });
-        navigator.msSaveOrOpenBlob(blob, filename);
-    } else {
-        // For other browsers
-        downloadLink.href = 'data:' + dataType + ', ' + tableHTML;
-        downloadLink.download = filename;
-        downloadLink.click();
-    }
-
-    // Cleanup
-    document.body.removeChild(downloadLink);
-}
-
+    
+        
     
